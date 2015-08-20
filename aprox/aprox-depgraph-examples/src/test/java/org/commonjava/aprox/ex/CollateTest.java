701ac8e45cdb8a48ea273ec18ee7a99046f29bd3
John Casey <jdcasey@commonjava.org>
2015-08-20 13:54:34 -0500

    Run gist methods as unit tests to make them easy to try out...still having some trouble with embedded server shutdown, though.

---
+++ aprox/aprox-depgraph-examples/src/test/java/org/commonjava/aprox/ex/CollateTest.java
@@ -0,0 +1,19 @@
+package org.commonjava.aprox.ex;
+
+import org.junit.Test;
+
+/**
+ * Created by jdcasey on 8/20/15.
+ */
+public class CollateTest
+        extends AbstractGistTest
+{
+
+    @Test
+    public void collateByScmUrlAndGroupId()
+            throws Exception
+    {
+        gist.collateByScmUrlAndGroupId();
+    }
+
+}
---
+++ aprox/aprox-depgraph-examples/src/test/java/org/commonjava/aprox/ex/ExportGraphTest.java
@@ -0,0 +1,19 @@
+package org.commonjava.aprox.ex;
+
+import org.junit.Test;
+
+/**
+ * Created by jdcasey on 8/20/15.
+ */
+public class ExportGraphTest
+        extends AbstractGistTest
+{
+
+    @Test
+    public void exportGraph()
+            throws Exception
+    {
+        gist.exportGraph();
+    }
+
+}
